f76b73e30c4a5a6d8a34cf87868f61314fcd910c
Taiwo Ajiboye <boldrackdev@gmail.com>
2024-10-10 00:01:01 +0100

    test: see dxtc work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,14 @@ export default function Home() {
         <div className=" p-1 px-2 bg-slate-700">NORMAL</div>
         <span><span className="text-green-500 mr-1">:</span><span className="text-slate-100 mr-2">recording</span><span className="text-red-400">@q</span></span>
       </main>
+
+        <iframe id="dextools-widget"
+    title="DEXTools Trading Chart"
+    width="500"
+    height="400" src="https://www.dextools.io/widget-chart/en/ether/pe-light/0xa29fe6ef9592b5d408cca961d0fb9b1faf497d6d?theme=light&chartType=2&chartResolution=30&drawingToolbars=false"></iframe>
+
+
+
     </div>
   );
 }
